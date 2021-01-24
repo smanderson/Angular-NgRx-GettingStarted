21cc6b5fe615ba02c9b0981053b64cae00d959a9
Sean Anderson <Sean Anderson>
2021-01-24 18:38:12 +0000

    Implement observable state in user component
    (use async pipe to remove need managing subscriptions)

--- APM-Demo0/src/app/user/login.component.ts
+++ APM-Demo0/src/app/user/login.component.ts
@@ -7,6 +7,7 @@ import { State } from '../state/app.state';
 import { AuthService } from './auth.service';
 import { getMaskUserName } from './state/user.reducer';
 import * as UserActions from './state/user.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './login.component.html',
@@ -15,17 +16,14 @@ import * as UserActions from './state/user.actions';
 export class LoginComponent implements OnInit {
   pageTitle = 'Log In';
 
-  maskUserName: boolean;
+  maskUserName$: Observable<boolean>;
 
   constructor(private authService: AuthService,
               private router: Router,
               private store: Store<State>) { }
 
   ngOnInit(): void {
-    // TODO: unsubscribe
-    this.store.select(getMaskUserName).subscribe(isMasked => 
-        this.maskUserName = isMasked
-    );
+    this.maskUserName$ = this.store.select(getMaskUserName);
   }
 
   cancel(): void {
