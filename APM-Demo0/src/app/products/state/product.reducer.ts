e028f1095d9be7d05ebdd7fe3266ca4e219e2cb7
Sean Anderson <Sean Anderson>
2021-01-24 14:41:38 +0000

    Remove console.log from product reducer

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -4,11 +4,8 @@ export const productReducer = createReducer(
     {
         showProductCode: true
     },
-    on(createAction('[Product] Toggle Product Code'), state => {
-        console.log('original state: ' + JSON.stringify(state));
-        return {
-            ...state,
-            showProductCode: !state.showProductCode
-        };
-    })
+    on(createAction('[Product] Toggle Product Code'), state => ({
+        ...state,
+        showProductCode: !state.showProductCode
+    }))
 );
